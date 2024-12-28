e5a15982ccd7bb51b1706012d84dc055fcc4614e
LaPepega <tmv03092008@gmail.com>
2024-12-28 20:47:30 +0500

    address as 1st arg

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+
 #include "server.h"
 #include "client.h"
 
@@ -7,9 +10,24 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Initializing server...");
+    if (argc < 2)
+    {
+        perror("No address specified");
+        exit(1);
+    }
+    struct in_addr con_addr;
+
+    if (inet_pton(AF_INET, argv[1], &con_addr) < 1)
+    {
+        perror("Invalid address");
+        exit(1);
+    }
+
+    printf("Initializing server...\n");
     int server_socket = server_init_socket(SERVER_PORT);
-    printf("Initializing client...");
+
+    printf("Initializing client...\n");
     int client_socket = client_init_socket(CLIENT_PORT);
+
     return 0;
 }
